6bb5442d2dee2aca429ccad05b9dc1aae0145dad
Edgar Mendoza <mledgaro@gmail.com>
2023-01-20 08:59:54 -0600

    Movie model

---
+++ MovieRecords/Models/Movie.swift
@@ -0,0 +1,19 @@
+//
+//  Movie.swift
+//  MovieRecords
+//
+//  Created by MAC on 20/01/23.
+//
+
+import Foundation
+
+struct Movie {
+    
+    var id: String
+    var rank: Int
+    var title: String
+    var year: String
+    var imageUrl: String
+    var crew: String
+    var imdbRating: Float
+}
