e940eb08c3dfcec7be7b69be7ea462df3ecae973
Edgar Mendoza <mledgaro@gmail.com>
2023-01-20 08:40:57 -0600

    First commit

---
+++ MovieRecords/MovieRecordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieRecordsApp.swift
+//  MovieRecords
+//
+//  Created by MAC on 20/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieRecordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
