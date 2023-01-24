adaa069b8bbc6c7a0d1cebc1859da6669231630e
Edgar Mendoza <mledgaro@gmail.com>
2023-01-23 21:51:19 -0600

    Cache top movies file

--- MovieRecords/ViewModels/IMDbAPI.swift
+++ MovieRecords/ViewModels/IMDbAPI.swift
@@ -16,15 +16,30 @@ class IMDbAPI: ObservableObject {
     private static let baseUrl = "https://imdb-api.com/en/API/"
     private static let apiKey = "k_alt156cp"
     
+    private var cachesDir: URL {
+        try! FileManager.default.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+    }
+    
+    private var topFile: URL {
+        cachesDir.appendingPathComponent("top").appendingPathExtension(for: .json)
+    }
     
     @Published var movies: [Movie]
     
     
     init() {
         
-        self.movies = Array(repeating: Movie.SHAWSHANK_REDEMPTION, count: 10)
+        self.movies = []
         
-        self.top250()
+        if FileManager.default.fileExists(atPath: String(topFile.path)) {
+            
+            let topData = try! Data(contentsOf: topFile)
+            self.movies = try! JSONDecoder().decode([Movie].self, from: topData)
+            
+        } else {
+            self.top250()
+        }
+        
     }
     
     func top250() {
@@ -33,14 +48,16 @@ class IMDbAPI: ObservableObject {
         
         AF.request(url).responseDecodable(of: APIResponseArray.self) { response in
 
-            self.movies = response.value?.items ?? Array(repeating: Movie.SHAWSHANK_REDEMPTION, count: 10)
+            self.movies = response.value?.items ?? []
+            let data = try! JSONEncoder().encode(self.movies)
+            try! data.write(to: self.topFile)
         }
     }
     
-    static func movieDetails(imdbId: String) async throws {
-        
-        let url = "\(IMDbAPI.baseUrl)Title/\(IMDbAPI.apiKey)/\(imdbId)/FullActor,FullCast,Trailer,"
-        
-    }
+//    static func movieDetails(imdbId: String) async throws {
+//
+//        let [url = "\(IMDbAPI.baseUrl)Title/\(IMDbAPI.apiKey)/\(imdbId)/FullActor,FullCast,Trailer,"
+//
+//    }
     
 }
